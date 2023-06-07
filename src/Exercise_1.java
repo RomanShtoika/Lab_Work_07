7cb6bc27ddebfb1d889f1d596fef9bbfc1fe852f
RomanShtoika <roman.shtoika.tr.2021@lpnu.ua>
2023-06-07 19:43:56 +0300

    Lab7

--- src/Exercise_1.java
+++ src/Exercise_1.java
@@ -1,2 +1,176 @@
-package PACKAGE_NAME;public class Exercise_1 {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+class Car {
+    private final String brand;
+    private double enginePower;
+    private Driver driver;
+    private double price;
+    private final int year;
+
+    public Car(String brand, double enginePower, Driver driver, double price, int year) {
+        this.brand = brand;
+        this.enginePower = enginePower;
+        this.driver = driver;
+        this.price = price;
+        this.year = year;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public double getEnginePower() {
+        return enginePower;
+    }
+
+    public void setEnginePower(double enginePower) {
+        this.enginePower = enginePower;
+    }
+
+    public Driver getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Driver driver) {
+        this.driver = driver;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getYear() {
+        return year;
+    }
+}
+
+class Driver {
+    private final String name;
+    private final int age;
+    private int drivingExperience;
+
+    public Driver(String name, int age, int drivingExperience) {
+        this.name = name;
+        this.age = age;
+        this.drivingExperience = drivingExperience;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getDrivingExperience() {
+        return drivingExperience;
+    }
+
+    public void increaseDrivingExperience() {
+        drivingExperience++;
+    }
+}
+
+class TaxiServiceHelper {
+    public static Car getRandomCar(List<Car> cars) {
+        Random random = new Random();
+        int index = random.nextInt(cars.size());
+        return cars.get(index);
+    }
+}
+
+public class Exercise_1{
+    public static void main(String[] args) {
+        List<Car> cars = new ArrayList<>();
+
+        // Створення 12 машинок
+        Driver driver1 = new Driver("Taras", 45, 6);
+        Car car1 = new Car("Honda", 137, driver1, 8500, 2011);
+        cars.add(car1);
+
+        Driver driver2 = new Driver("Yaroslav", 35, 1);
+        Car car2 = new Car("Lexus", 120, driver2, 15000, 2012);
+        cars.add(car2);
+
+        Driver driver3 = new Driver("David", 29, 5);
+        Car car3 = new Car("Dacia", 96, driver3, 4500, 2009);
+        cars.add(car3);
+
+        Driver driver4 = new Driver("Valentin", 24, 7);
+        Car car4 = new Car("Mitsubishi", 140, driver4, 11000, 2008);
+        cars.add(car4);
+
+        Driver driver5 = new Driver("Andriy", 25, 5);
+        Car car5 = new Car("Chevrolet", 107, driver5, 6800, 2007);
+        cars.add(car5);
+
+        Driver driver6 = new Driver("Roman", 20, 4);
+        Car car6 = new Car("Tavria", 69, driver6, 1500, 2003);
+        cars.add(car6);
+
+        Driver driver7 = new Driver("Orest", 23, 4);
+        Car car7 = new Car("Honda", 165, driver7, 11000, 2013);
+        cars.add(car7);
+
+        Driver driver8 = new Driver("Svitlana", 21, 3);
+        Car car8 = new Car("Nissan", 115, driver8, 9000, 2013);
+        cars.add(car8);
+
+        Driver driver9 = new Driver("Bohdan", 19, 6);
+        Car car9 = new Car("Skoda", 137, driver9, 8000, 2012);
+        cars.add(car9);
+
+        Driver driver10 = new Driver("Edward", 23, 4);
+        Car car10 = new Car("Opel", 115, driver10, 6000, 2002);
+        cars.add(car10);
+
+        Driver driver11 = new Driver("Yura", 36, 5);
+        Car car11 = new Car("BMW", 137, driver11, 9500, 2003);
+        cars.add(car11);
+
+        Driver driver12 = new Driver("Olena", 19, 5);
+        Car car12 = new Car("Peugeot", 110, driver12, 8000, 2008);
+        cars.add(car12);
+
+        // Ремонт половини автопарку
+        for (int i = 0; i < cars.size() / 2; i++) {
+            Car car = cars.get(i);
+            double increasedPower = car.getEnginePower() * 1.1;
+            car.setEnginePower(increasedPower);
+
+            Driver newDriver = new Driver("Новий водій " + (i + 1), 25, 1);
+            car.setDriver(newDriver);
+        }
+
+        // Підвищення потужності та ціни кожної другої машини
+        for (int i = 1; i < cars.size(); i += 2) {
+            Car car = cars.get(i);
+            double increasedPower = car.getEnginePower() * 1.1;
+            double increasedPrice = car.getPrice() * 1.05;
+            car.setEnginePower(increasedPower);
+            car.setPrice(increasedPrice);
+        }
+
+        // Перевірка досвіду водіїв та відправлення на курси
+        for (Car car : cars) {
+            Driver driver = car.getDriver();
+            if (driver.getDrivingExperience() < 5 && driver.getAge() > 25) {
+                driver.increaseDrivingExperience();
+            }
+        }
+
+        // Випадковий вибір машинки
+        Car randomCar = TaxiServiceHelper.getRandomCar(cars);
+        System.out.println("Автомобіль марки " + randomCar.getBrand() + " з водієм " + randomCar.getDriver().getName() + " виїхав за вами.");
+
+        // Виведення повідомлення про прибуття водія
+        System.out.println("Водій " + randomCar.getDriver().getName() + " прибув на місце.");
+    }
 }
--- src/Exercise_2.java
+++ src/Exercise_2.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class Exercise_2 {
+import java.util.HashMap;
+import java.util.Map;
+
+public class Exercise_2 {
+    public static void main(String[] args) {
+        // Створення об'єкту HashMap для зберігання прізвищ та номерів телефонів
+        Map<String, String> phoneBook = new HashMap<>();
+
+        // Додавання елементів в карту
+        phoneBook.put("Дрозд", "+380964234367");
+        phoneBook.put("Грищук", "+380671251569");
+        phoneBook.put("Щербань", "+380638434561");
+        phoneBook.put("Юрчук", "+380981544564");
+        phoneBook.put("Іванчук", "+38097882155");
+        phoneBook.put("Хомич", "+380687444567");
+        phoneBook.put("Гайдай", "+3806812532569");
+        phoneBook.put("Штойка", "+380508664562");
+        phoneBook.put("Гонтар", "+380960991568");
+        phoneBook.put("Миколенко", "+380631550560");
+
+        // Виведення інформації з книги за допомогою циклу
+        for (Map.Entry<String, String> entry : phoneBook.entrySet()) {
+            String lastName = entry.getKey();
+            String phoneNumber = entry.getValue();
+            System.out.println("Прізвище: " + lastName + ", Номер телефону: " + phoneNumber);
+        }
+
+        // Знайдення номеру за прізвищем
+        String lastNameToFind = "Штойка";
+        if (phoneBook.containsKey(lastNameToFind)) {
+            String phoneNumber = phoneBook.get(lastNameToFind);
+            System.out.println("Номер телефону для прізвища " + lastNameToFind + ": " + phoneNumber);
+        } else {
+            System.out.println("У книзі відсутній абонент з прізвищем " + lastNameToFind);
+        }
+
+        // Видалення запису з книги
+        String lastNameToRemove = "Хомич";
+        phoneBook.remove(lastNameToRemove);
+        System.out.println("Запис з прізвищем " + lastNameToRemove + " видалено.");
+
+        // Виведення кількості записів у книзі
+        int numberOfEntries = phoneBook.size();
+        System.out.println("Кількість записів у книзі: " + numberOfEntries);
+    }
 }
+
